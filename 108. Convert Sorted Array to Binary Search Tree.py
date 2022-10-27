5317d70c64f0a44970d89e0ad5b99ab2c2bb56b3
sunjin7725 <sunjin7725@gmail.com>
2022-10-27 13:56:32 +0900

    ~2022-10-25

---
+++ 108. Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-  
+""" 
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+"""
+
+nums = [-10, -3, 0, 5, 9]
